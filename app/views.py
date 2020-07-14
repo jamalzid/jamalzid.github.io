b9bb0fc89c3055d9b8156794922dbdb5bb10c757
jamalzid <aitzidan@gmail.com>
2020-07-14 09:31:36 +0100

    initial commit

--- app/views.py
+++ app/views.py
@@ -5,19 +5,20 @@ from .forms import TaskForm
 
 
 def index(request):
-    tasks=Task.objects.all()
-    form=TaskForm()
-    if request.method=='POST':
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/')
 
-    context={
-        'tasks':tasks,
+    context = {
+        'tasks': tasks,
         'form': form,
     }
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
+
 
 def edite(request,id):
     task=get_object_or_404(Task,id=id)
